0d21063d175cd60b653a73418c8e0a689e9de8ae
Rodrigo <rodrigo_santos01@hotmail.com.br>
2022-07-27 21:07:09 -0300

    27/07 21:07h

---
+++ python3_Exercicios/ex012.py
@@ -0,0 +1,2 @@
+p = float(input('Product value? :'))
+print('discounted value is :{}'.format(p-(p*0.05)))
---
+++ python3_Exercicios/ex013.py
@@ -0,0 +1,2 @@
+p = float(input('Current wage? R$:'))
+print('New wage R$:{:.2f}'.format(p+(p*0.15)))
