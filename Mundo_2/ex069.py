6484fd3c2c754350168e73ceb5fed72b25aa20e3
Rodri <rodrigo_santos01@hotmail.com.br>
2023-03-07 04:41:25 -0300

    Atualização...

--- Mundo_2/ex069.py
+++ Mundo_2/ex069.py
@@ -1,18 +1,30 @@
 homens = mulheres20 = maioridade = 0
 while True:
+    print('...........Novo Cadastro............')
     try:
         idade = int(input('Idade: '))
     except:
         continue
     sexo = ' '
     while sexo not in 'FM':
-        sexo = input('Sexo: ').strip().upper()[0]
+        try:
+            sexo = input('Sexo: ').strip().upper()[0]
+        except:
+            continue
     if idade > 18:
         maioridade += 1
+    if sexo == 'M':
+        homens += 1
+    if sexo == 'F' and idade < 20:
+        mulheres20 += 1
     print('Cadastro realizado com sucesso')
     print('-='*15)
     op = ' '
     while op not in 'SN':
-        op = input('Que continuar? [S/N] ? ').strip().upper()[0]
+        try:
+            op = input('Que continuar? [S/N] ? ').strip().upper()[0]
+        except:
+            continue
     if op == 'N':
         break
+print(f'{maioridade} {homens} {mulheres20}')
