20b2f201d527546521560aac7cc2b36330e57fd8
Rodrigo <rodrigo_santos01@hotmail.com.br>
2024-11-26 21:14:40 -0300

    Update do ex 088...

--- Exercicios/ex088.py
+++ Exercicios/ex088.py
@@ -1,24 +1,20 @@
 from random import randint
 from time import sleep
-contador = 0
-cont_list = 0
-rep = []
 jogos = []
 descarte = []
-n = 10 #int(input('Quantos jogos serão gerados: '))
+n = int(input('Quantos jogos serão gerados: '))
+n2 = n
 for p in range(n):
     descarte.clear()
     while len(descarte) < 6:
         n = randint(1, 60)
-        if n in descarte:
-            contador += 1
-            print(f'Aqui foi gerado  o {contador}ª numero repetido {n}, ')
         if n not in descarte:
             descarte.append(n)
-            print(descarte)
     jogos.append(descarte[:])
 
-print("\n\n\n\n*/*/*/*/*/*/*/* 30")
+print(f'Gerando {n2} jogos...\n')
+sleep(3)
 for c in jogos:
     print(sorted(c))
     sleep(1)
+print('\n Muito obrigado por utilizar nosso gerador de apostas, fique bem e volte sempre que precisar.')
